54e0f1ffcfc45ce65ceabe562b2dd5cebe640208
qqbuby <qqbuby@gmail.com>
2016-12-22 10:53:45 +0800

    Add BooleanExtensions.cs

---
+++ src/Alyio.Extensions/Extensions/BooleanExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+
+namespace Alyio.Extensions
+{
+    public static class BooleanExtensions
+    {
+        /// <summary>
+        /// Converts the value of a specified object to an equivalent Boolean value.
+        /// </summary>
+        /// <param name="value">The object to convert. </param>
+        /// <returns>
+        /// <see cref="System.Object"/>: true or false, which reflects the value returned by invoking the <see cref="IConvertible.ToBoolean"/> method for the underlying type of value. If value is null, the method returns false. 
+        /// <see cref="System.String"/>: true if value equals TrueString, or false if value equals FalseString or null.
+        /// <see cref="System.Double"/>: true if value is not zero; otherwise, false.
+        /// </returns>
+        public static bool ToBoolean(this object value)
+        {
+            if (value == null) { return false; }
+            if (typeof(bool).Equals(value.GetType())) { return (bool)value; }
+            var converter = value as IConvertible;
+            if (converter != null)
+            {
+                try
+                {
+                    return converter.ToBoolean(CultureInfo.InvariantCulture);
+                }
+                catch
+                {
+                    var d = value.ToDouble();
+                    if (d != null)
+                    {
+                        return d != 0D;
+                    }
+                    else
+                    {
+                        var s = value.ToString();
+                        return s.ToBoolean();
+                    }
+                }
+            }
+            else
+            {
+                var d = value.ToDouble();
+                if (d != null)
+                {
+                    return d != 0D;
+                }
+                else
+                {
+                    var s = value.ToString();
+                    return s.ToBoolean();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts the specified string representation of a logical value to its <see cref="System.Boolean"/> equivalent.
+        /// </summary>
+        /// <param name="s">A string containing the value to convert.</param>
+        /// <returns>true if value is equal to <see cref="System.Boolean.TrueString"/> or false if value is equal to <see cref="System.Boolean.FalseString"/>, otherwise false.</returns>
+        public static bool ToBoolean(this string s)
+        {
+            bool result;
+            if (bool.TryParse(s, out result))
+            {
+                return result;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ test/Alyio.Extensions.Tests/BooleanExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alyio.Extensions.Tests
+{
+    public class BooleanExtensionsTests
+    {
+        [Fact]
+        public void Object_Tests()
+        {
+            Assert.True(bool.TrueString.ToBoolean());
+            Assert.False(bool.FalseString.ToBoolean());
+
+            Assert.True(1.0D.ToBoolean());
+            Assert.False(0.0D.ToBoolean());
+
+            Assert.True(true.ToBoolean());
+            Assert.False(false.ToBoolean());
+
+            object t = true;
+            Assert.True(t.ToBoolean());
+            object f = false;
+            Assert.False(f.ToBoolean());
+
+            object t1 = "1";
+            Assert.True(t1.ToBoolean());
+            object t2 = "0";
+            Assert.False(t2.ToBoolean());
+
+            var mdnz = new MyDoubleNotZero();
+            Assert.True(mdnz.ToBoolean());
+            var mdz = new MyDoubleZero();
+            Assert.False(mdz.ToBoolean());
+        }
+
+        class MyDoubleZero
+        {
+            public override string ToString()
+            {
+                return "0.0";
+            }
+        }
+
+        class MyDoubleNotZero
+        {
+            public override string ToString()
+            {
+                return "1.0";
+            }
+        }
+    }
+}
